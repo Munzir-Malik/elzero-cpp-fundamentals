c9d6db13d13bf271d63d3f73224d2b109b5cfa92
Munzir <>
2025-09-29 18:26:20 +0200

    Upload solution file

---
+++ Website-Assignments/Week2-Assignments/Assignment14/solution.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // Write Your Code Here
+  int num;
+  cout << "Enter a number: ";
+  cin >> num;
+  cout << "Result is: " << num * num;
+
+  // Num 10
+  // Needed Output 100 [10 * 10]
+  return 0;
+}
