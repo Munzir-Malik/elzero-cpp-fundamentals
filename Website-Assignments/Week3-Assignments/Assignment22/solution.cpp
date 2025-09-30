719468193ab0a35c611ba6fecf63d24784e2deb5
Munzir <>
2025-09-30 20:17:07 +0200

    Upload solution file

---
+++ Website-Assignments/Week3-Assignments/Assignment22/solution.cpp
@@ -0,0 +1,37 @@
+# include <iostream>
+using namespace std; 
+
+int main () {
+
+  int a = 69;
+  int b = 108;
+  int c = 122;
+  int d = 101;
+  int e = 114;
+  int f = 111;
+
+
+  // Convert the six variables to characters
+  cout << "Character Of This ASCII Value 69 Is: " << char(a) << endl;
+  cout << "Character Of This ASCII Value 108 Is: " << char(b) << endl;
+  cout << "Character Of This ASCII Value 122 Is: " << char(c) << endl;
+  cout << "Character Of This ASCII Value 101 Is: " << char(d) << endl;
+  cout << "Character Of This ASCII Value 114 Is: " << char(e) << endl;
+  cout << "Character Of This ASCII Value 111 Is: " << char(f) << endl;
+
+
+  // Print Elzero using the six variables
+  cout << char(a) << char(b) << char(c) << char(d) << char(e) << char(f);
+
+  return 0;
+}
+// Output Needed
+// "Character Of This ASCII Value 69 Is E"
+// "Character Of This ASCII Value 108 Is l"
+// "Character Of This ASCII Value 122 Is z"
+// "Character Of This ASCII Value 101 Is e"
+// "Character Of This ASCII Value 114 Is r"
+// "Character Of This ASCII Value 111 Is o"
+
+// // Output Needed In Last Line
+// "Elzero"
