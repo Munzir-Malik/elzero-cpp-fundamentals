385e2d7d8df283e14013f52eeb31ad263d0c0483
Munzir <>
2025-09-30 20:16:19 +0200

    Upload solution file

---
+++ Website-Assignments/Week3-Assignments/Assignment21/solution.cpp
@@ -0,0 +1,17 @@
+# include <iostream>
+using namespace std;
+
+int main () 
+{
+  char a = '~';
+  char b = '&';
+  char c = '%';
+  char d = 'A';
+
+  cout << "ASCII Value of ~ is: " << int(a) << "\n";
+  cout << "ASCII Value of & is: " << int(b) << "\n";
+  cout << "ASCII Value of % is: " << int(c) << "\n";
+  cout << "ASCII Value of A is: " << int(d) << "\n";
+
+  return 0;
+}
